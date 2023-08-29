58932374dde02ecdd520e2a109d87d4f48442854
Barabbus <andrewclarke022@gmail.com>
2023-08-29 19:55:54 +0100

    Update checkPermissions.js

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,6 +1,6 @@
 import { UnAuthenticatedError } from '../errors/index.js'
 
-const checkPermissions = (requestUser, resourceUserId) => {
+const checkPermissions = (requestUser, resourceUserId ) => {
     if (requestUser.userId === resourceUserId.toString()) return
 
     throw new UnAuthenticatedError("Not authorized to access this route")
